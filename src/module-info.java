0a508f0825a8f62458bf2082f3f2879bde33bb8b
Ian Daelman <ian.daelman@student.hogent.be>
2023-02-21 17:13:19 +0100

    module-info.java aangepast met de nodige packages

--- src/module-info.java
+++ src/module-info.java
@@ -6,4 +6,18 @@
  *
  */
 module B2B_Portal_Delaware {
+	// FX
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	// Persistence
+	requires java.persistence;
+	requires java.sql;
+	requires java.instrument;
+	// Unit tests
+	requires org.junit.jupiter.api;
+	requires org.mockito.junit.jupiter;
+	requires org.mockito;
+
 }
