73698407318ce1c54e237b55eb773ce4a75ecf79
Ian Daelman <ian.daelman@student.hogent.be>
2023-03-05 15:26:19 +0100

    Gui: Formulier toegevoegd WIP

--- src/gui/BeheerTransportdienstSchermController.java
+++ src/gui/BeheerTransportdienstSchermController.java
@@ -8,13 +8,18 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import repository.TransportdienstDTO;
 import repository.UserDTO;
 
@@ -28,6 +33,7 @@ public class BeheerTransportdienstSchermController extends Pane {
 	private TableColumn<TransportdienstDTO, String> naamKolom;
 	private TableColumn<TransportdienstDTO, Boolean> statusKolom;
 	private TabPane tabPane;
+	private GridPane gridPane;
 
 	private DomeinController dc;
 
@@ -52,32 +58,70 @@ public class BeheerTransportdienstSchermController extends Pane {
 
 	//TODO welkomnaam implementeren
 	private void buildGuid(List<TransportdienstDTO> transportdienstDTOLijst, UserDTO user) {
-		
+		//Initialisatie van de verschillende elementen op het scherm
 		welkomNaam = new Label();
 		tableViewTransportdienst = new TableView<TransportdienstDTO>();
 		naamKolom = new TableColumn<TransportdienstDTO, String>("Naam");
 		statusKolom = new TableColumn<TransportdienstDTO, Boolean>("Status");
 		tabPane = new TabPane();
+		gridPane = new GridPane();
 		Tab toevoegenTab = new Tab("Toevoegen");
 		Tab raadpleegTab = new Tab("Raadplegen");
 		
+		
+		
+		
+		
+		Label headerLabel = new Label("TOEVOEGEN TRANSPORTDIENST");
+		Label lblNaamTransportdienst = new Label("Naam transportdienst:");
+		Label lblContactpersoon = new Label("Contactpersoon:");
+		Label lblTelefoonnummer = new Label("Telefoonnummer:");
+		Label lblEmailadres = new Label("Emailadres: ");
+		TextField txtNaamTransportdienst = new TextField();
+		TextField txtContactpersoon = new TextField();
+		TextField txtTelefoonnummer = new TextField();
+		TextField txtEmailadres = new TextField();
+	    
+	    gridPane.setHgap(10);
+	    gridPane.setVgap(10);
+	    gridPane.setPadding(new Insets(10,10,10,10));
+	    
+	    gridPane.add(headerLabel, 0,0,2,1);
+	    gridPane.add(lblNaamTransportdienst, 0, 1,1,1);
+	    gridPane.add(txtNaamTransportdienst, 1, 1,1,1);
+	    gridPane.add(lblContactpersoon, 0, 2,1,1);
+	    gridPane.add(txtContactpersoon, 1, 2,1,1);
+	    gridPane.add(lblTelefoonnummer, 0, 3,1,1);
+	    gridPane.add(txtTelefoonnummer, 1, 3,1,1);
+	    gridPane.add(lblEmailadres, 0, 4,1,1);
+	    gridPane.add(txtEmailadres, 1, 4,1,1);
+	    
+	    headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+		
+		toevoegenTab.setClosable(false);
+		raadpleegTab.setClosable(false);
+		
+		toevoegenTab.setContent(gridPane);
+		
 		tabPane.getTabs().addAll(toevoegenTab, raadpleegTab);
 		
+		//Dimensies en styling instellen van de veschillende tabs
 		tabPane.setLayoutX(400);
 		tabPane.setLayoutY(170);
 		
 		tabPane.prefHeightProperty().set(900);
 		tabPane.prefWidthProperty().set(1500);
 		
-		welkomNaam.setText(String.format("Welkom %s %s", user.getVoornaam(), user.getFamilienaam()));
-
-		ObservableList<TransportdienstDTO> list = FXCollections.observableArrayList(transportdienstDTOLijst);
-
+		tabPane.setStyle("-fx-border-style:solid; -fx-padding: 1;");
 		
 		tableViewTransportdienst.setLayoutX(85);
 		tableViewTransportdienst.setLayoutY(170);
 		
 		tableViewTransportdienst.prefHeightProperty().set(900);
+		
+		welkomNaam.setText(String.format("Welkom %s %s", user.getVoornaam(), user.getFamilienaam()));
+
+		ObservableList<TransportdienstDTO> list = FXCollections.observableArrayList(transportdienstDTOLijst);
 		
 		naamKolom.setCellValueFactory(new PropertyValueFactory<TransportdienstDTO, String>("naam"));
 		statusKolom.setCellValueFactory(new PropertyValueFactory<TransportdienstDTO, Boolean>("isActief"));
