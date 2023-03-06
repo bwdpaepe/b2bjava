f279511ec42ccce2be360253a386bf27a504405c
Ian Daelman <ian.daelman@student.hogent.be>
2023-03-06 20:30:41 +0100

    Raadpleeg tab toegevoegd WIP

--- src/gui/BeheerTransportdienstSchermController.java
+++ src/gui/BeheerTransportdienstSchermController.java
@@ -39,7 +39,8 @@ public class BeheerTransportdienstSchermController extends Pane {
 	private TableColumn<TransportdienstDTO, String> naamKolom;
 	private TableColumn<TransportdienstDTO, Boolean> statusKolom;
 	private TabPane tabPane;
-	private GridPane gridPane;
+	private GridPane gridPaneToevoegen;
+	private GridPane gridPaneRaadplegen;
 	private DomeinController dc;
 	private ObservableList<TransportdienstDTO> list;
 
@@ -109,8 +110,10 @@ public class BeheerTransportdienstSchermController extends Pane {
 		// Tab om transportdiensten toe te voegen
 		Tab toevoegenTab = new Tab("Toevoegen");
 
+		toevoegenTab.setClosable(false);
+
 		// Opbouw tabblad toevoegen
-		gridPane = new GridPane();
+		gridPaneToevoegen = new GridPane();
 		Label lblTitel = new Label("TOEVOEGEN TRANSPORTDIENST");
 		Label lblNaamTransportdienst = new Label("Naam transportdienst:");
 		Label lblContactpersoonVoornaam = new Label("Contactpersoon voornaam:");
@@ -147,31 +150,31 @@ public class BeheerTransportdienstSchermController extends Pane {
 		}
 
 		// Opbouw gridPane tabblad Toevoegen
-		gridPane.setHgap(10);
-		gridPane.setVgap(10);
-		gridPane.setPadding(new Insets(10, 10, 10, 10));
+		gridPaneToevoegen.setHgap(10);
+		gridPaneToevoegen.setVgap(10);
+		gridPaneToevoegen.setPadding(new Insets(10, 10, 10, 10));
 
-		gridPane.add(lblTitel, 0, 0, 2, 1);
-		gridPane.add(lblNaamTransportdienst, 0, 1, 1, 1);
-		gridPane.add(txtNaamTransportdienst, 1, 1, 1, 1);
-		gridPane.add(lblContactpersoonVoornaam, 0, 2, 1, 1);
-		gridPane.add(txtContactpersoonVoornaam, 1, 2, 1, 1);
-		gridPane.add(lblContactpersoonFamilienaam, 0, 3, 1, 1);
-		gridPane.add(txtContactpersoonFamilienaam, 1, 3, 1, 1);
-		gridPane.add(lblTelefoonnummer, 0, 4, 1, 1);
-		gridPane.add(txtTelefoonnummer, 1, 4, 1, 1);
-		gridPane.add(lblEmailadres, 0, 5, 1, 1);
-		gridPane.add(txtEmailadres, 1, 5, 1, 1);
-		gridPane.add(lblTrackAndTrace, 0, 6, 2, 1);
-		gridPane.add(lblVerificatiecode, 0, 7, 1, 1);
-		gridPane.add(cbVerificatiecode, 1, 7, 1, 1);
-		gridPane.add(lblBarcodeLengteCode, 0, 8, 1, 1);
-		gridPane.add(cbBarcodeLengte, 1, 8, 1, 1);
-		gridPane.add(lblBarcodePrefix, 0, 9, 1, 1);
-		gridPane.add(txtBarcodePrefix, 1, 9, 1, 1);
-		gridPane.add(lblIsBarcodeEnkelCijfers, 0, 10, 1, 1);
-		gridPane.add(cbIsBarcodeEnkelCijfers, 1, 10, 1, 1);
-		gridPane.add(btnToevoegen, 0, 11, 2, 2);
+		gridPaneToevoegen.add(lblTitel, 0, 0, 2, 1);
+		gridPaneToevoegen.add(lblNaamTransportdienst, 0, 1, 1, 1);
+		gridPaneToevoegen.add(txtNaamTransportdienst, 1, 1, 1, 1);
+		gridPaneToevoegen.add(lblContactpersoonVoornaam, 0, 2, 1, 1);
+		gridPaneToevoegen.add(txtContactpersoonVoornaam, 1, 2, 1, 1);
+		gridPaneToevoegen.add(lblContactpersoonFamilienaam, 0, 3, 1, 1);
+		gridPaneToevoegen.add(txtContactpersoonFamilienaam, 1, 3, 1, 1);
+		gridPaneToevoegen.add(lblTelefoonnummer, 0, 4, 1, 1);
+		gridPaneToevoegen.add(txtTelefoonnummer, 1, 4, 1, 1);
+		gridPaneToevoegen.add(lblEmailadres, 0, 5, 1, 1);
+		gridPaneToevoegen.add(txtEmailadres, 1, 5, 1, 1);
+		gridPaneToevoegen.add(lblTrackAndTrace, 0, 6, 2, 1);
+		gridPaneToevoegen.add(lblVerificatiecode, 0, 7, 1, 1);
+		gridPaneToevoegen.add(cbVerificatiecode, 1, 7, 1, 1);
+		gridPaneToevoegen.add(lblBarcodeLengteCode, 0, 8, 1, 1);
+		gridPaneToevoegen.add(cbBarcodeLengte, 1, 8, 1, 1);
+		gridPaneToevoegen.add(lblBarcodePrefix, 0, 9, 1, 1);
+		gridPaneToevoegen.add(txtBarcodePrefix, 1, 9, 1, 1);
+		gridPaneToevoegen.add(lblIsBarcodeEnkelCijfers, 0, 10, 1, 1);
+		gridPaneToevoegen.add(cbIsBarcodeEnkelCijfers, 1, 10, 1, 1);
+		gridPaneToevoegen.add(btnToevoegen, 0, 11, 2, 2);
 
 		// Events tabblad toevoegen
 		btnToevoegen.setOnAction(evt -> {
@@ -213,13 +216,107 @@ public class BeheerTransportdienstSchermController extends Pane {
 			tableViewTransportdienst.setItems(list);
 		});
 
-		toevoegenTab.setClosable(false);
-		toevoegenTab.setContent(gridPane);
+		toevoegenTab.setContent(gridPaneToevoegen);
 
 		// Tab om transportdiensten te raadplegen en aan te passen
 		Tab raadpleegTab = new Tab("Raadplegen");
 
 		raadpleegTab.setClosable(false);
+
+		// Opbouw tabblad toevoegen
+		gridPaneRaadplegen = new GridPane();
+		Label lblTitelRaadpleegTab = new Label("RAADPLEGEN TRANSPORTDIENST");
+		Label lblNaamTransportdienstRaadpleegTab = new Label("Naam transportdienst:");
+		Label lblContactpersoonVoornaamRaadpleegTab = new Label("Contactpersoon voornaam:");
+		Label lblContactpersoonFamilienaamRaadpleegTab = new Label("Contactpersoon familienaam:");
+		Label lblTelefoonnummerRaadpleegTab = new Label("Telefoonnummer:");
+		Label lblEmailadresRaadpleegTab = new Label("Emailadres: ");
+		Label lblTrackAndTraceRaadpleegTab = new Label("TRACK AND TRACE CODE");
+		Label lblVerificatiecodeRaadpleegTab = new Label("Verificatiecode:");
+		Label lblBarcodeLengteCodeRaadpleegTab = new Label("Aantal karakters van de code:");
+		Label lblBarcodePrefixRaadpleegTab = new Label("Track and Trace prefix: ");
+		Label lblIsBarcodeEnkelCijfersRaadpleegTab = new Label("Bestaat de code enkel uit cijfers:");
+		Label lblVerificatiecodeRaadpleegTabtd = new Label("HIER KOMT DE VERIFICATIECODE VAN DE GESELECTEERDE TD");
+		Label lblBarcodePrefixRaadpleegTabtd = new Label("HIER KOMT DE BARCODE PREFIX VAN DE GESELECTEERDE TD");
+
+		Label lblBarcodeLengteRaadpleegTabtd = new Label("HIER KOMT DE BARCODE LENGTE VAN DE GESELECTEERDE TD");
+		Label lblIsBarcodeEnkelCijfersRaadpleegTabtd = new Label(
+				"HIER KOMT DE BARCODE ENKEL CIJFERS VAN DE GESELECTEERDE TD");
+		TextField txtNaamTransportdienstRaadpleegTab = new TextField();
+		TextField txtContactpersoonVoornaamRaadpleegTab = new TextField();
+		TextField txtContactpersoonFamilienaamRaadpleegTab = new TextField();
+		TextField txtTelefoonnummerRaadpleegTab = new TextField();
+		TextField txtEmailadresRaadpleegTab = new TextField();
+		TextField txtBarcodePrefixRaadpleegTab = new TextField();
+		ChoiceBox<String> cbVerificatiecodeRaadpleegTab = new ChoiceBox<>();
+		ChoiceBox<Integer> cbBarcodeLengteRaadpleegTab = new ChoiceBox<>();
+		CheckBox cbIsBarcodeEnkelCijfersRaadpleegTab = new CheckBox();
+		Button btnAanpassen = new Button("AANPASSEN TRANSPORTDIENST");
+
+		// Styling gripane elementen
+		btnAanpassen.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+		lblTitelRaadpleegTab.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+		lblTrackAndTraceRaadpleegTab.setFont(Font.font("Arial", 24));
+
+		// Opties toevoegen aan gridPane elementen
+		cbVerificatiecodeRaadpleegTab.getItems().add("Postcode");
+		cbVerificatiecodeRaadpleegTab.getItems().add("Orderid");
+
+		for (int i = 1; i < 21; i++) {
+			cbBarcodeLengteRaadpleegTab.getItems().add(i);
+		}
+
+		// Opbouw gridPane tabblad Toevoegen
+		gridPaneRaadplegen.setHgap(10);
+		gridPaneRaadplegen.setVgap(10);
+		gridPaneRaadplegen.setPadding(new Insets(10, 10, 10, 10));
+
+		gridPaneRaadplegen.add(lblTitelRaadpleegTab, 0, 0, 2, 1);
+		gridPaneRaadplegen.add(lblNaamTransportdienstRaadpleegTab, 0, 1, 1, 1);
+		gridPaneRaadplegen.add(txtNaamTransportdienstRaadpleegTab, 1, 1, 1, 1);
+		gridPaneRaadplegen.add(lblContactpersoonVoornaamRaadpleegTab, 0, 2, 1, 1);
+		gridPaneRaadplegen.add(txtContactpersoonVoornaamRaadpleegTab, 1, 2, 1, 1);
+		gridPaneRaadplegen.add(lblContactpersoonFamilienaamRaadpleegTab, 0, 3, 1, 1);
+		gridPaneRaadplegen.add(txtContactpersoonFamilienaamRaadpleegTab, 1, 3, 1, 1);
+		gridPaneRaadplegen.add(lblTelefoonnummerRaadpleegTab, 0, 4, 1, 1);
+		gridPaneRaadplegen.add(txtTelefoonnummerRaadpleegTab, 1, 4, 1, 1);
+		gridPaneRaadplegen.add(lblEmailadresRaadpleegTab, 0, 5, 1, 1);
+		gridPaneRaadplegen.add(txtEmailadresRaadpleegTab, 1, 5, 1, 1);
+		gridPaneRaadplegen.add(lblTrackAndTraceRaadpleegTab, 0, 6, 2, 1);
+		gridPaneRaadplegen.add(lblVerificatiecodeRaadpleegTab, 0, 7, 1, 1);
+		gridPaneRaadplegen.add(lblVerificatiecodeRaadpleegTabtd, 1, 7, 1, 1);
+		gridPaneRaadplegen.add(cbVerificatiecodeRaadpleegTab, 1, 7, 1, 1);
+		gridPaneRaadplegen.add(lblBarcodeLengteCodeRaadpleegTab, 0, 8, 1, 1);
+		gridPaneRaadplegen.add(cbBarcodeLengteRaadpleegTab, 1, 8, 1, 1);
+		gridPaneRaadplegen.add(lblBarcodeLengteRaadpleegTabtd, 1, 8, 1, 1);
+		gridPaneRaadplegen.add(lblBarcodePrefixRaadpleegTab, 0, 9, 1, 1);
+		gridPaneRaadplegen.add(txtBarcodePrefixRaadpleegTab, 1, 9, 1, 1);
+		gridPaneRaadplegen.add(lblBarcodePrefixRaadpleegTabtd, 1, 9, 1, 1);
+		gridPaneRaadplegen.add(lblIsBarcodeEnkelCijfersRaadpleegTab, 0, 10, 1, 1);
+		gridPaneRaadplegen.add(cbIsBarcodeEnkelCijfersRaadpleegTab, 1, 10, 1, 1);
+		gridPaneRaadplegen.add(lblIsBarcodeEnkelCijfersRaadpleegTabtd, 1, 10, 1, 1);
+		gridPaneRaadplegen.add(btnAanpassen, 0, 11, 2, 2);
+
+		txtNaamTransportdienstRaadpleegTab.setEditable(false);
+		txtContactpersoonVoornaamRaadpleegTab.setEditable(false);
+		txtContactpersoonFamilienaamRaadpleegTab.setEditable(false);
+		txtTelefoonnummerRaadpleegTab.setEditable(false);
+		txtEmailadresRaadpleegTab.setEditable(false);
+
+		// TODO deze 4 velden een label toevoegen met de waarde van de geselecteerde td,
+		// bij drukken button aanpassen kan je deze zaken terug visible zetten
+		cbVerificatiecodeRaadpleegTab.setVisible(false);
+		txtBarcodePrefixRaadpleegTab.setVisible(false);
+		cbBarcodeLengteRaadpleegTab.setVisible(false);
+		cbIsBarcodeEnkelCijfersRaadpleegTab.setVisible(false);
+
+		raadpleegTab.setContent(gridPaneRaadplegen);
+		
+		btnAanpassen.setOnAction(evt -> {
+			//TODO drukken button moeten alle velden editable zijn
+			//Placeholder moet nog een worden toegevoegd
+			//Event van tablview moet nog gemaakt worden
+		});
 
 		tabPane.getTabs().addAll(toevoegenTab, raadpleegTab);
 
