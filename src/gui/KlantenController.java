88c4a84b55ea63a8b3cb282f3380abc1432d907b
DIMITRI VALCKENIER <dimitri.valckenier@student.hogent.be>
2023-03-10 16:28:20 +0100

    begin uitwerking ophalen klantenlist

--- src/gui/KlantenController.java
+++ src/gui/KlantenController.java
@@ -1,13 +1,29 @@
 package gui;
 
+
+
+import java.io.IOException;
+
 import domein.DomeinController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import repository.KlantLijstEntryDTO;
 
 
 public class KlantenController extends Pane {
 	private DomeinController dc;
 
-	
+	@FXML
+	private Label errorMessage;
+	@FXML
+	private Pane errorWindow;
+	@FXML
+	private Button closeButton;
 	
 	public KlantenController() {
 
@@ -19,5 +35,23 @@ public class KlantenController extends Pane {
 		
 		this.dc = dc;
 	}
+	
+	@FXML
+	public void onEnter(ActionEvent ae) throws IOException {
+		this.geKlantenList(ae);
+	}
 
+	private void geKlantenList(ActionEvent ae) throws IOException {
+		try
+		{
+			ObservableList<KlantLijstEntryDTO> klantenList = FXCollections.observableList(dc.geefLijstVanKlantenMetAantalOpenstaandeBestellingen());
+			
+			System.out.println(klantenList);
+		} catch (Exception e)
+		{
+			errorMessage.setText(e.getMessage());
+			this.errorWindow.setVisible(true);
+			this.closeButton.setVisible(true);
+		}
+	}
 }
