3686a62048d6772a4ae18075ddc3613d7c29a748
DIMITRI VALCKENIER <dimitri.valckenier@student.hogent.be>
2023-03-13 21:26:17 +0100

    toevoegen subtotaal in BesteldProductDTO

--- src/repository/BesteldProductDTO.java
+++ src/repository/BesteldProductDTO.java
@@ -8,12 +8,14 @@ public class BesteldProductDTO {
 	protected final String naam;
 	protected final double eenheidsprijs;
 	protected final int aantal;
+	protected final double subtotaal;
 	
 	public BesteldProductDTO(BesteldProduct besteldProduct) {
 		this.id = besteldProduct.getId();
 		this.naam = besteldProduct.getNaam();
 		this.eenheidsprijs = besteldProduct.getEenheidsprijs();
 		this.aantal = besteldProduct.getAantal();
+		this.subtotaal = besteldProduct.getEenheidsprijs() * besteldProduct.getAantal();
 	}
 	
 	public long getId() {
@@ -31,4 +33,11 @@ public class BesteldProductDTO {
 	public int getAantal() {
 		return aantal;
 	}
+
+	public double getSubtotaal()
+	{
+		return subtotaal;
+	}
+	
+	
 }
