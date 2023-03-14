a68378b86b507a1ed23a987f378d58b2778d828c
Ian Daelman <ian.daelman@student.hogent.be>
2023-03-14 08:08:23 +0100

    email validatie toegevoegd

--- src/service/ValidationService.java
+++ src/service/ValidationService.java
@@ -1,91 +1,82 @@
 package service;
 
 import java.util.Date;
+import java.util.Set;
 
 import javax.persistence.Transient;
 
-public class ValidationService
-{
+import repository.ContactpersoonDTO;
+
+public class ValidationService {
 	@Transient
 	public static final int MIN_PW_LENGTH = 8; // TODO afspreken met team
 
-	public static final void controleerNietBlanco(Object waarde)
-	{
-		if (waarde == null)
-		{
+	public static final void controleerNietBlanco(Object waarde) {
+		if (waarde == null) {
 			throw new IllegalArgumentException("Veld is verplicht");
 		}
-		if(waarde instanceof String && ((String) waarde).isBlank()) {
+		if (waarde instanceof String && ((String) waarde).isBlank()) {
 			throw new IllegalArgumentException("Veld is verplicht");
 		}
 	}
-	
+
 	public static final void controleerGroterDanNul(int waarde) {
-		if (waarde <= 0)
-		{// eventueel nog andere checks toevoegen
+		if (waarde <= 0) {// eventueel nog andere checks toevoegen
 			throw new IllegalArgumentException("Veld moet groter zijn dan nul");
 		}
 	}
+
 	public static final void controleerGroterDanNul(long waarde) {
-		if (waarde <= 0)
-		{// eventueel nog andere checks toevoegen
+		if (waarde <= 0) {// eventueel nog andere checks toevoegen
 			throw new IllegalArgumentException("Veld moet groter zijn dan nul");
 		}
 	}
-	
+
 	public static final void controleerGroterDanNul(double waarde) {
-		if (waarde <= 0)
-		{// eventueel nog andere checks toevoegen
+		if (waarde <= 0) {// eventueel nog andere checks toevoegen
 			throw new IllegalArgumentException("Veld moet groter zijn dan nul");
 		}
 	}
 
-
-	public static final void controleerEmail(String email)
-	{
-		if (email == null || email.isBlank())
-		{
+	public static final void controleerEmail(String email) {
+		if (email == null || email.isBlank()) {
 			throw new IllegalArgumentException("E-mailadres is verplicht");
 		}
 		// https://howtodoinjava.com/java/regex/java-regex-validate-email-address/
 		if (!email.matches(
-				"^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$"))
-		{
+				"^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")) {
 			throw new IllegalArgumentException("Ongeldig e-mailadres formaat");
 		}
 	}
 
-	public static final void controleerWachtwoord(String password)
-	{
+	public static final void controleerWachtwoord(String password) {
 		// error als geen wachtwoord, te kort, of bevat spatie
-		if (password == null || password.length() < MIN_PW_LENGTH || password.matches(".*\\s.*"))
-		{
+		if (password == null || password.length() < MIN_PW_LENGTH || password.matches(".*\\s.*")) {
 			throw new IllegalArgumentException("Wachtwoord is ongeldig");
 		}
 	}
 
-	public static final void controleerTelefoonnummer(String telefoonnummer)
-	{
+	public static final void controleerTelefoonnummer(String telefoonnummer) {
 		// error als geen telefoonnummer, of niet voldoet aan de regex: eventueel
 		// beginnen met een '+', en verder enkel cijfers (minstens 6)
-		if (telefoonnummer == null || !telefoonnummer.matches("^\\+?\\d{6,}$"))
-		{
+		if (telefoonnummer == null || !telefoonnummer.matches("^\\+?\\d{6,}$")) {
 			throw new IllegalArgumentException("Telefoonnummer is ongeldig");
 		}
 	}
-	
-	//uiteindelijk nog niet nodig, maar laat het staan voor eventueel later gebruik
+
+	// uiteindelijk nog niet nodig, maar laat het staan voor eventueel later gebruik
 	public static final void controleerDatumNietInVerleden(Date datum) {
 		if (new Date().compareTo(datum) > 0) {
 			throw new IllegalArgumentException("datum kan niet in het verleden liggen");
 		}
 	}
 
-
-	
-
-
-
-
+	public static final void controleerUniekEmailadres(Set<ContactpersoonDTO> contactpersonen, String emailadres) {
+		for (ContactpersoonDTO c : contactpersonen) {
+			if (c.getEmailAdres().equals(emailadres)) {
+				throw new IllegalArgumentException("Iedere contactpersoon moet een uniek emailadres hebben");
+			}
+		}
+	}
 
 }
