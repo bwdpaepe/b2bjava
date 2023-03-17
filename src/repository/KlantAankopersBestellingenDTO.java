f34d85aa03caa42e2aa3fad203e8cf1f87fb7b90
DIMITRI VALCKENIER <dimitri.valckenier@student.hogent.be>
2023-03-17 22:10:36 +0100

    DTO voor details van klant

--- src/repository/KlantAankopersBestellingenDTO.java
+++ src/repository/KlantAankopersBestellingenDTO.java
@@ -1,6 +1,7 @@
 package repository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import domein.Bedrijf;
 import domein.Bestelling;
@@ -11,11 +12,15 @@ public class KlantAankopersBestellingenDTO
 
 	private final Long klantId;
 	private final String klantNaam;
+	private final List<AankoperDetailsDTO> aankopers;
+	private final List<BestellingDetailsDTO> bestellingen;
 
 	public KlantAankopersBestellingenDTO(Bedrijf klant, List<User> aankopers, List<Bestelling> bestellingen)
 	{
 		this.klantId = klant.getID();
 		this.klantNaam = klant.getNaam();
+		this.aankopers = aankopers.stream().map(el -> new AankoperDetailsDTO(el)).collect(Collectors.toUnmodifiableList());
+		this.bestellingen = bestellingen.stream().map(el -> new BestellingDetailsDTO(el)).collect(Collectors.toUnmodifiableList());
 	}
 
 	public Long getKlantId()
@@ -28,5 +33,21 @@ public class KlantAankopersBestellingenDTO
 		return klantNaam;
 	}
 
+	public List<AankoperDetailsDTO> getAankopers()
+	{
+		return aankopers;
+	}
+
+	public List<BestellingDetailsDTO> getBestellingen()
+	{
+		return bestellingen;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "KlantAankopersBestellingenDTO [klantId=" + klantId + ", klantNaam=" + klantNaam + ", aankopers="
+				+ aankopers + ", bestellingen=" + bestellingen + "]";
+	}
 
 }
