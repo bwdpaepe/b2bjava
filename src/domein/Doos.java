ec6805ddd45fc434c3e4783c8441575ec6079084
JoachimDauchot <dauchot.joachim@gmail.com>
2023-03-26 15:41:52 +0200

    dozen laden

--- src/domein/Doos.java
+++ src/domein/Doos.java
@@ -12,6 +12,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -21,6 +23,9 @@ import service.ValidationService;
 @Table(name = "Dozen", uniqueConstraints = {
         @UniqueConstraint(columnNames = {"bedrijf_id", "naam"})
 })
+@NamedQueries({
+	@NamedQuery(name = "Doos.getDozenVanBedrijf", query = "select d from Doos d where d.bedrijf = :bedrijf")})
+
 public class Doos implements Serializable {
 
 	private static final long serialVersionUID = 1L;
--- src/gui/DozenController.java
+++ src/gui/DozenController.java
@@ -1,10 +1,58 @@
 package gui;
 
+import java.util.Date;
+
 import domein.DomeinController;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.StackPane;
+import javafx.util.Callback;
+import repository.BestellingDetailsDTO;
+import repository.DoosDTO;
+import repository.KlantLijstEntryDTO;
 
 public class DozenController {
 	
 	private DomeinController dc;
+	
+	
+	@FXML
+	private TableView<DoosDTO> tvDozen;
+	@FXML
+	TableColumn<DoosDTO, String> naamColumn;
+	@FXML
+	TableColumn<DoosDTO, String> typeColumn;
+	@FXML
+	TableColumn<DoosDTO, Number> lengteColumn;
+	@FXML
+	TableColumn<DoosDTO, Number> breedteColumn;
+	@FXML
+	TableColumn<DoosDTO, Number> hoogteColumn;
+	@FXML
+	TableColumn<DoosDTO, Number> prijsColumn;
+	@FXML
+	TableColumn<DoosDTO, String> isActiefColumn;
+	@FXML
+	TableColumn<DoosDTO, Boolean> editColumn;
+	@FXML
+	TableColumn<Button, Button> deleteColumn;
+
 
 	public DozenController() {
 		// TODO Auto-generated constructor stub
@@ -16,7 +64,69 @@ public class DozenController {
 	}
 	
 	public void loadDozen() {
-		//TODO		
+	    //System.out.println("Klanten lijst loading");
+	    ObservableList<DoosDTO> dozenList = FXCollections.observableList(dc.getDozen());
+
+	    naamColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNaam()));
+	    typeColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDoosType()));
+	    lengteColumn.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getLengte()));
+	    breedteColumn.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getBreedte()));
+	    hoogteColumn.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getHoogte()));
+	    prijsColumn.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getPrijs()));
+	    isActiefColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().isActief()? "Ja" : "Nee"));
+	    
+	    FilteredList<DoosDTO> filteredDozenList = new FilteredList<>(dozenList, p -> true);
+	    
+	    SortedList<DoosDTO> sortedDozenList = new SortedList<>(filteredDozenList);
+	    sortedDozenList.comparatorProperty().bind(tvDozen.comparatorProperty());
+	    
+	    tvDozen.setItems(sortedDozenList);
+	    
+	    editColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<DoosDTO, Boolean>, ObservableValue<Boolean>>() {
+
+			@Override
+			public ObservableValue<Boolean> call(CellDataFeatures<DoosDTO, Boolean> features) {
+				return new SimpleBooleanProperty(features.getValue() != null);
+			}
+	    });
+	    
+	    editColumn.setCellFactory(new Callback<TableColumn<DoosDTO,Boolean>, TableCell<DoosDTO,Boolean>>() {
+	    	@Override public TableCell<DoosDTO, Boolean> call(TableColumn<DoosDTO, Boolean> doosBooleanTableColumn){
+	    		return new ButtonCell(tvDozen);
+	    	}
+		});
+
+
+
+
+	}
+	
+	private class ButtonCell extends TableCell<DoosDTO, Boolean>{
+		final Button button = new Button("Wijzig");
+		final StackPane paddedButton = new StackPane();
+		
+		
+		private ButtonCell(TableView<DoosDTO> table){
+		      paddedButton.setPadding(new Insets(3));
+		      paddedButton.getChildren().add(button);
+		      button.getStyleClass().add("tvButton");
+			
+		}
+		
+		
+		
+	    @Override protected void updateItem(Boolean item, boolean empty) {
+	        super.updateItem(item, empty);
+	        if (!empty) {
+	          setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+	          setGraphic(paddedButton);
+	        } else {
+	          setGraphic(null);
+	        }
+	      }
+	    }
 	}
 
-}
+
+
+
